64936cb5fa89d0e6fba6fafef7584df25194a966
Jane Brooks <jane@deerjane.com>
2022-04-14 15:18:51 -0700

    updated texted

--- js/app.js
+++ js/app.js
@@ -78,9 +78,9 @@ function updateStatusText(isOwner, checking) {
   const statusText = document.querySelector('.owner-status');
   if(checking) {
     if(isOwner) {
-      statusText.innerText = `You do own ${COLLECTION_NAME}!! 😻 Let's see how many${renderDots(dots)}`;
+      statusText.innerText = `You do own ${COLLECTION_NAME}!! Let's see how many${renderDots(dots)}`;
     } else {
-      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} 😻${renderDots(dots)}`;
+      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} ${renderDots(dots)}`;
     }
   } else {
     if(isOwner) {
